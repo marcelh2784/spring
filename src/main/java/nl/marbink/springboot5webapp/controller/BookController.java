4deb18f94dd6b67e3b094da9e6dfacf5c4d69746
MH <marcelh2784@gmail.com>
2018-03-21 22:59:30 +0100

    Chapter 21: Configuring Spring MVC Controllers

---
+++ src/main/java/nl/marbink/springboot5webapp/controller/BookController.java
@@ -0,0 +1,20 @@
+package nl.marbink.springboot5webapp.controller;
+
+import nl.marbink.springboot5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
