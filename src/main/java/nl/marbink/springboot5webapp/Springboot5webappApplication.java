569d2d814b19ea0ad7e925277a29fb621d88e964
MH <marcelh2784@gmail.com>
2018-03-28 17:37:02 +0200

    refactor

--- src/main/java/nl/marbink/springboot5webapp/Springboot5webappApplication.java
+++ src/main/java/nl/marbink/springboot5webapp/Springboot5webappApplication.java
@@ -2,8 +2,11 @@ package nl.marbink.springboot5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//TODO mvn spring-boot:run -Dspring-boot.run.arguments=--debug; show all autoconfiguration classes
 @SpringBootApplication
+@ComponentScan(basePackages = {"nl.marbink"})
 public class Springboot5webappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/nl/marbink/springboot5webapp/controller/AuthorController.java
+++ src/main/java/nl/marbink/springboot5webapp/controller/AuthorController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+//TODO Spring Stereotypes: @Component, @Controller, @Service, @Repository, @RestController
 @Controller
 public class AuthorController {
 
